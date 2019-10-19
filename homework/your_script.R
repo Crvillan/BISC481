ddfcf8f6b1107e5952c31a1a2ec898d1a7b36ef3
Crvillan <56176339+Crvillan@users.noreply.github.com>
2019-10-19 14:31:05 -0700

    Update your_script.R

--- homework/your_script.R
+++ homework/your_script.R
@@ -1,1 +1,24 @@
+## Question 5.
+##Install packages
+# Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install()
+# DNAshapeR
+BiocManager::install("DNAshapeR")
+# Caret
+install.packages("caret")
 
+## Initialization
+library(DNAshapeR)
+library(caret)
+workingPath <- "/Users/ceciliavillanueva/Desktop/BISC481/CTCF/"
+
+# Extract sample sequences
+fn <- system.file("extdata", "CGRsample.fa", package = "DNAshapeR")
+
+# Predict DNA shapes
+pred <- getShape(fn)
+
+# Generate ensemble plots
+plotShape(pred$MGW)
