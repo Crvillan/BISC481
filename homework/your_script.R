c9dc0c0f23acaa0155be30938303abb11d88e363
Crvillan <56176339+Crvillan@users.noreply.github.com>
2019-10-20 23:23:11 -0700

    Cecilia V.

--- homework/your_script.R
+++ homework/your_script.R
@@ -1,4 +1,92 @@
 ## Cecilia Villanueva Code
+
+##Question 4- Using MLR to predict binding sites
+
+## Install packages
+# Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install()
+# DNAshapeR
+BiocManager::install("DNAshapeR")
+# Caret
+install.packages("caret")
+install.packages ('data.table')
+
+## Initialization
+library(DNAshapeR)
+library(caret)
+workingPath <- "/Users/ceciliavillanueva/Desktop/BISC481/gcPBM/"
+
+## Predict DNA shapes
+fn_fasta <- paste0(workingPath, "Mad.txt.fa")
+pred <- getShape(fn_fasta)
+
+## Encode feature vectors
+featureType <- c("1-mer", "1-shape")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Mad.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+                method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
+result
+
+###To generate the plot comparing the two methods we use the following code which takes into account that x-axis/values = "1-mer" and y-axis/values = "1-mer" + "1-shape"
+## Install and initialize packages
+install.packages("ggplot2")
+install.packages("grid")
+library(ggplot2)
+library(grid)
+
+## Theme
+my.theme <- theme(
+  plot.margin = unit(c(0.1, 0.5, 0.1, 0.1), "cm"),
+  axis.text.x = element_text(colour="black", size=12),
+  axis.text.y = element_text(colour="black", size=12),
+  axis.title.x = element_text(colour="black", size=12),
+  axis.title.y = element_text(colour="black", size=12),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.background = element_blank(),
+  axis.line = element_line(colour = "black"),
+  axis.text = element_text(colour ="black"),
+  axis.ticks = element_line(colour = "black")
+)
+
+#DATA TO INPUT
+#Mad: 1-mer only R=0.854836, 1-mer+1-shape  R= 0.8639159
+#Max: 1-mer only R=0.854836, 1-mer+1-shape R=0.8644361
+#Myc: 1-mer only R=0.854836, 1-mer+1-shape R=0.854836
+## Data preparation
+one-mer_Rsquared <- c(0.854836,0.854836,0.854836)
+one-mer+shape_Rsquared <- c(0.8639159,0.8644361, 0.854836)
+
+## Ploting
+ggplot() +
+  geom_point(aes(x = one-mer_Rsquared, y = one-mer+shape_Rsquared), color = "red", size=1) +
+  geom_abline(slope=1) + geom_vline(xintercept=0) + geom_hline(yintercept=0) +
+  coord_fixed(ratio = 1, xlim = c(0,1), ylim = c(0,1)) +
+  scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) +
+  my.theme
+
+################## New Problem
 ##Question 5
 ##Install packages
 # Bioconductor
